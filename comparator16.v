fea496ae50f56ba2387ca05e53651ca6eb9d19ba
Mohanraj <117710598+mj1069@users.noreply.github.com>
2024-01-23 19:03:52 +0530

    Update comparator16.v

--- comparator16.v
+++ comparator16.v
@@ -1,12 +1,16 @@
-module comparator16(
+module comparator16 (
   input [15:0] in1, in2,
   output cmp_out
 );
+  
+  reg cmp_out_reg;
 
-  always(in1, in2) begin
-    if(in1>in2)
-      cmp_out <= 1'b1;
+  always @(in1, in2) begin
+    if(in1 > in2)
+      cmp_out_reg <= 1'b1;
     else
-      cmp_out <= 1'b0;
+      cmp_out_reg <= 1'b0;
   end
+  
+  assign cmp_out = cmp_out_reg;
 endmodule
