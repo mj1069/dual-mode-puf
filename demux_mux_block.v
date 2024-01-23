b0cc3e1c880d8e968ab1a1a75cea94fac4e56908
Mohanraj <117710598+mj1069@users.noreply.github.com>
2024-01-23 18:36:21 +0530

    Create demux_mux_block.v

---
+++ demux_mux_block.v
@@ -0,0 +1,11 @@
+module mux_block(
+  input in_up,in_down,sel,
+  output out_up,out_down
+);
+
+  demux1x2 demux_up(in_up, sel, demux_up_out0, demux_up_out1);
+  mux2x1 mux_up(demux_up_out0, demux_up_out1, sel ,out_up);
+  demux1x2 demux_down(in_down, sel, demux_down_out0, demux_down_out1);
+  mux2x1 mux_down(demux_down_out0, demux_down_out1, sel, out_down);
+  
+endmodule
